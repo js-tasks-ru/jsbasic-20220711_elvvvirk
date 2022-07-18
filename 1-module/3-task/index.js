6f6569366dda3f45d1888d8ab6f04fae9b96c803
elviraff <elvvvirk@mail.ru>
2022-07-18 18:53:12 +0200

    Lesson 1 Task 3

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,11 @@
-function ucFirst(str) {
-  // ваш код...
+'use strict'
+function ucFirst(str){
+    if (str === '') return str
+    let newFirstSymbol = "";
+    newFirstSymbol = str[0].toUpperCase();
+    str = str.slice(1);
+    str = newFirstSymbol + str;
+    return str
 }
+
+alert (ucFirst(''))
