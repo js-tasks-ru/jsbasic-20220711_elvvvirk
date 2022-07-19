9074c4c21227aa749d806b746cf2c54795fa7e59
elviraff <elvvvirk@mail.ru>
2022-07-19 20:20:40 +0200

    lesson 3 task 3 was corrected

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,10 +1,7 @@
 'use strict'
 function ucFirst(str){
-    if (str === '') return str
-    let newFirstSymbol = "";
-    newFirstSymbol = str[0].toUpperCase();
-    str = str.slice(1);
-    str = newFirstSymbol + str;
+    if (str === '') return str;
+    str = str[0].toUpperCase() + str.slice(1);
     return str
 }
 
