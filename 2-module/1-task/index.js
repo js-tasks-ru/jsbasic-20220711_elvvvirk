4ee086ebeca0f8b024689336774c6a24cbbf4799
elviraff <elvvvirk@mail.ru>
2022-07-22 00:33:32 +0200

    module 2 task 1

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,8 @@
 function sumSalary(salaries) {
-  // ваш код...
+    sum = 0;
+    for (let key in salaries){
+      if (salaries[key] && isFinite(salaries[key]))
+        sum += salaries[key]
+    }
+    return sum;
 }
