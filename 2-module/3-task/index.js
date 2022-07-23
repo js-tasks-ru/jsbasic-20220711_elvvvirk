9400bd8ccdf9ee788d84cec585a34a6f2b66172d
elviraff <elvvvirk@mail.ru>
2022-07-24 00:10:30 +0200

    module 2 task 3

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,19 @@
+let sum = 0;
+let mul = 1;
 let calculator = {
   // ваш код
+  a: null,
+  b: null,
+  read(){
+    this.a = +prompt('enter a:', 0)
+    this.b = +prompt('enter b:', 0)
+  },
+  sum() {
+      return sum = this.a + this.b
+  },
+  mul(){
+      return mul = this.a * this.b
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
