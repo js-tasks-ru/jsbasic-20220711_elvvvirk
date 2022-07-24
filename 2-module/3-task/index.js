e8d3de7d26e5bdf7549d1f9ecfe39684fee19940
elviraff <elvvvirk@mail.ru>
2022-07-24 18:17:34 +0200

    module 2 task 3 - changed

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,18 +1,14 @@
-let sum = 0;
-let mul = 1;
 let calculator = {
   // ваш код
-  a: null,
-  b: null,
-  read(){
-    this.a = +prompt('enter a:', 0)
-    this.b = +prompt('enter b:', 0)
+  read(a,b){
+    this.a = a
+    this.b = b
   },
   sum() {
-      return sum = this.a + this.b
+      return this.a + this.b
   },
   mul(){
-      return mul = this.a * this.b
+      return this.a * this.b
   }
 };
 
