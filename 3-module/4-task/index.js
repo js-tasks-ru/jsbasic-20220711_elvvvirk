0ec23103bfa60627787931aeda764af6a6695753
elviraff <elvvvirk@mail.ru>
2022-07-31 21:27:17 +0200

    module 3 task 4

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,9 @@
-function showSalary(users, age) {
-  // ваш код...
+
+function showSalary(users, age){
+  let str = ""
+  users.filter(item => item["age"] <= age).forEach((item, index, array) => {
+    str += item["name"] + ", " + item["balance"]
+    if (index != array.length - 1) str += "\n"
+  });
+  return str
 }
