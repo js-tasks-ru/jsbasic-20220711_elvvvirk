7617a34f9877fadaf7ccbcb2c87cacb81d87b07b
elviraff <elvvvirk@mail.ru>
2022-08-03 00:45:07 +0200

    module 4 task 2

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,7 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  for (let item of table.rows){
+    for (let key of item.cells)
+        if (key.cellIndex == item.rowIndex) 
+        table.rows[key.cellIndex].cells[item.rowIndex].style.backgroundColor = 'red'
+}
 }
