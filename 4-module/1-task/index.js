5267caed17f94fa92fef78a5879553f49785431a
elviraff <elvvvirk@mail.ru>
2022-08-02 23:39:27 +0200

    module 4 task 1

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul')
+  let str =""
+  for (let key of friends){
+    str += `<li> ${key.firstName} ${key.lastName} </li>` +'\n'
+  }
+  ul.innerHTML = str
+  return ul
 }
