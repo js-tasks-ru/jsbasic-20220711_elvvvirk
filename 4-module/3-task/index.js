0498001f0478fbe87395a3ef3aadbd6871a06b38
elviraff <elvvvirk@mail.ru>
2022-08-03 15:26:05 +0200

    module 4 task 3

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,29 @@
 function highlight(table) {
-  // ваш код...
+  for (let item of table.rows){
+    //check attr data-availabel
+        let attr = item.querySelector('[data-available]')
+        console.log(attr)
+    console.log(item.textContent)
+    if (attr != null){
+        let cellValue = (attr.dataset.available)
+        if (cellValue == "true"){ 
+            item.classList.add('available')
+        } 
+        else if (cellValue == "false")
+        item.classList.add('unavailable')
+    }
+    else {
+        if (item.rowIndex != 0) item.hidden = true
+        }
+    //check attr male
+    for (let key of item.cells){
+            if (key.textContent == 'm') item.classList.add('male')
+            if (key.textContent == 'f') item.classList.add('female')
+    //add attr style
+            if(+key.textContent < 18) item.style.textDecoration = "line-through"
+        }
+    
+    }
 }
+
+
